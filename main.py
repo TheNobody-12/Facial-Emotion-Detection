e39397ef851a6f2760e333e1ca0dedfe399fa8e4
TheNobody-12 <sarthak.kce20@sot.pdpu.ac.in>
2022-09-26 08:11:19 +0530

    Sarthak commits 26Sept22

---
+++ main.py
@@ -0,0 +1,50 @@
+from tensorflow.keras.models import load_model
+from time import sleep
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image
+import cv2
+import numpy as np
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+import skimage.io
+import keras.backend as K
+import tensorflow as tf
+face_classifier = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
+classifier =load_model(r'vgg16EMD1.h5')
+
+class_labels=['angry','disgust','fear','happy','neutral','sad','surprise']
+cap=cv2.VideoCapture(0)
+
+while True:
+    ret,frame=cap.read()
+    labels=[]
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_classifier.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray=gray[y:y+h,x:x+w]
+        # covert 48,48,1 to 48,48,3
+        roi_gray = np.stack((roi_gray,)*3, axis=-1)
+        roi_gray=cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+
+        if np.sum([roi_gray])!=0:
+            roi=roi_gray.astype('float')/255.0
+            roi=img_to_array(roi)
+            roi=np.expand_dims(roi,axis=0)
+
+            preds=classifier.predict(roi)[0]
+            label=class_labels[preds.argmax()]
+            label_position=(x,y)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        else:
+            cv2.putText(frame,'No Face Found',(20,20),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+    
+    cv2.imshow('Emotion Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
